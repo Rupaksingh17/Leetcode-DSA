0f13d047020f77fe952e163b3f8c4b86c5a7e5c0
Rupaksingh17 <168707974+Rupaksingh17@users.noreply.github.com>
2026-03-11 01:58:35 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (48.48%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
+    void sortColors(vector<int>& arr) {
+   sort(arr.begin(),arr.end());
     }
 };
